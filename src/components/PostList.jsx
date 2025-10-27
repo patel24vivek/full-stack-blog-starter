4acac31301f743d28a343ff4678de473e62a23c8
patel24vivek <vivekpatel2472003@gmail.com>
2025-10-27 12:04:52 +0530

    Update PostList.jsx

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -5,10 +5,10 @@ const PostList = () => {
   return (
     <div className="flex flex-col gap-12 mb-8">
      <PostListItem item = 'postimg1.png'/>
-     <PostListItem item = 'logo.png'/>
-     <PostListItem item = 'logo.png'/>
-     <PostListItem item = 'logo.png'/>
-     <PostListItem item = 'logo.png'/>
+     <PostListItem item = 'postimg2.png'/>
+     <PostListItem item = 'postimg1.png'/>
+     <PostListItem item = 'postimg2.png'/>
+     <PostListItem item = 'postimg1.png'/>
     </div>
   )
 }
